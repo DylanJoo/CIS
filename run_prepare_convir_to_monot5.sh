67deac52fceb403b09c291abe2407360a9b13be0
DylanJoo <dylanjootw@gmail.com>
2022-08-08 21:26:07 +0800

    add quac dataset

--- run_prepare_convir_to_monot5.sh
+++ run_prepare_convir_to_monot5.sh
@@ -1,8 +1,19 @@
-python3 tools/convert_run_to_monot5.py \
-  -run spr/runs/dev.canard.rewrite.top1000.trec \
-  -corpus data/trec-car+marco-psg \
-  -k 1000 \
-  -q data/canard/dev.rewrite.tsv \
-  --output_text_pair dev.canard.rewrite.top1000.text_pair.txt \
-  --output_id_pair dev.canard.rewrite.top1000.id_pair.txt
+DIR=convir_data_construction
 
+# Rewrite retrieval with candidate from Answer (teacher)
+python3 tools/convert_run_to_monot5.py \
+  -run $DIR/runs/cast20.canard.train.answer.spr.top1000.trec \
+  -corpus data/cast20/collections/  \
+  -k 1000 \
+  -q data/canard/train.rewrite.tsv \
+  --output_text_pair canard.train.teacher.top1000.text_pair.txt \
+  --output_id_pair canard.train.teacher.top1000.id_pair.txt
+
+# Rewrite retrieval with candidate from Rewrite (student)
+python3 tools/convert_run_to_monot5.py \
+  -run $DIR/runs/cast20.canard.train.rewrite.spr.top1000.trec \
+  -corpus data/cast20/collections/  \
+  -k 1000 \
+  -q data/canard/train.rewrite.tsv \
+  --output_text_pair canard.train.student.top1000.text_pair.txt \
+  --output_id_pair canard.train.student.top1000.id_pair.txt
